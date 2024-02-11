6081d2bf35efa30b55abe10dae80721ed8cdfae9
Alexander Hack <alexander.m.hack@gmail.com>
2024-02-11 16:35:12 +0100

    UID

--- .strapi/client/app.js
+++ .strapi/client/app.js
@@ -4,6 +4,7 @@
  */
 import i18N from "@strapi/plugin-i18n/strapi-admin";
 import usersPermissions from "@strapi/plugin-users-permissions/strapi-admin";
+import strapiAdvancedUuid from "strapi-advanced-uuid/strapi-admin";
 import meilisearch from "strapi-plugin-meilisearch/strapi-admin";
 import { renderAdmin } from "@strapi/strapi/admin";
 
@@ -11,6 +12,7 @@ renderAdmin(document.getElementById("strapi"), {
   plugins: {
     i18n: i18N,
     "users-permissions": usersPermissions,
+    "strapi-advanced-uuid": strapiAdvancedUuid,
     meilisearch: meilisearch,
   },
 });
